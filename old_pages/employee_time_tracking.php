bd8e13ba7ddb162fda7a500728b7c11171181ec0
o-psi <andymalsb@gmail.com>
2024-11-09 12:06:10 -0600

    Remove old_pages directory from git tracking

--- old_pages/employee_time_tracking.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "/var/www/portal.twe.tech/includes/inc_all.php";
-
-
-
-require "/var/www/portal.twe.tech/includes/footer.php";
--- old_pages/settings/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once '/var/www/portal.twe.tech/includes/inc_all_settings.php';
-
-require_once '/var/www/portal.twe.tech/includes/footer.php';
