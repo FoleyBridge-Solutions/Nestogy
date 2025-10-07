271163bd016261c9f25b8cb204c5cf9a7594466c
Andrew Malsbury <andymalsb@gmail.com>
2025-10-07 07:58:59 +0000

    Fix: Add a "case default" clause to this "switch" statement.

--- app/Domains/PhysicalMail/Jobs/ProcessWebhookJob.php
+++ app/Domains/PhysicalMail/Jobs/ProcessWebhookJob.php
@@ -285,6 +285,13 @@ class ProcessWebhookJob implements ShouldQueue
             case 'returned_to_sender':
                 event(new \App\Domains\PhysicalMail\Events\MailOrderReturned($order));
                 break;
+
+            default:
+                Log::info('Unknown status, no event fired', [
+                    'order_id' => $order->id,
+                    'status' => $newStatus,
+                ]);
+                break;
         }
     }
 }
