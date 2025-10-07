81753e8adfe21f611a5de48780ed63123a70b042
Andrew Malsbury <andymalsb@gmail.com>
2025-10-07 03:26:04 +0000

    Fix: Add a <title> tag to this page.

--- resources/views/emails/daily-digest.blade.php
+++ resources/views/emails/daily-digest.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Daily Digest - {{ now()->format('F j, Y') }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
