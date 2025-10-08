73235b2366c5d9e7c28a9e5b34bcbe82a2d96394
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 18:40:56 +0000

    Fix: Delete this unreachable code or refactor the code to make it reachable.

--- resources/views/flux.backup/icon/chevron-up.blade.php
+++ resources/views/flux.backup/icon/chevron-up.blade.php
@@ -20,8 +20,7 @@ $classes = Flux::classes('shrink-0')
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
   <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
 </svg>
-
-        <?php break; ?>
+<?php break; ?>
 
     <?php case ('solid'): ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
