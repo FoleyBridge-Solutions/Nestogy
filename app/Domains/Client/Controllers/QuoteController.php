868da24613639ab062c1c4eed245ac11840f079b
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 18:21:40 -0500

    Fix: Define a constant instead of duplicating this literal "nullable|date" 5 times. (#2324)

--- app/Domains/Client/Controllers/QuoteController.php
+++ app/Domains/Client/Controllers/QuoteController.php
@@ -11,6 +11,8 @@ use Illuminate\Validation\Rule;
 
 class QuoteController extends Controller
 {
+    private const VALIDATION_NULLABLE_DATE = 'nullable|date';
+
     /**
      * Display a listing of all quotes (standalone view)
      */
@@ -117,10 +119,10 @@ class QuoteController extends Controller
             'discount_amount' => 'nullable|numeric|min:0',
             'discount_type' => 'nullable|in:'.implode(',', array_keys(ClientQuote::getDiscountTypes())),
             'valid_until' => 'nullable|date|after:today',
-            'issued_date' => 'nullable|date',
+            'issued_date' => self::VALIDATION_NULLABLE_DATE,
             'status' => 'required|in:'.implode(',', array_keys(ClientQuote::getStatuses())),
             'conversion_probability' => 'nullable|numeric|min:0|max:100',
-            'follow_up_date' => 'nullable|date',
+            'follow_up_date' => self::VALIDATION_NULLABLE_DATE,
             'terms_conditions' => 'nullable|string',
             'notes' => 'nullable|string',
             'payment_terms' => 'nullable|string',
@@ -246,11 +248,11 @@ class QuoteController extends Controller
             'tax_rate' => 'nullable|numeric|min:0|max:100',
             'discount_amount' => 'nullable|numeric|min:0',
             'discount_type' => 'nullable|in:'.implode(',', array_keys(ClientQuote::getDiscountTypes())),
-            'valid_until' => 'nullable|date',
-            'issued_date' => 'nullable|date',
+            'valid_until' => self::VALIDATION_NULLABLE_DATE,
+            'issued_date' => self::VALIDATION_NULLABLE_DATE,
             'status' => 'required|in:'.implode(',', array_keys(ClientQuote::getStatuses())),
             'conversion_probability' => 'nullable|numeric|min:0|max:100',
-            'follow_up_date' => 'nullable|date',
+            'follow_up_date' => self::VALIDATION_NULLABLE_DATE,
             'terms_conditions' => 'nullable|string',
             'notes' => 'nullable|string',
             'payment_terms' => 'nullable|string',
