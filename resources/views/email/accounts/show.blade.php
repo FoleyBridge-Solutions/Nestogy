764ebbf096cbf723796229f807fc50df7fa9fbe2
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 18:22:41 +0000

    Fix: A form label must be associated with a control.

--- resources/views/email/accounts/show.blade.php
+++ resources/views/email/accounts/show.blade.php
@@ -157,7 +157,7 @@
                                         <p class="mt-1 text-sm text-gray-900 dark:text-gray-100">{{ ucfirst($account->smtp_encryption) }}</p>
                                     </div>
                                     <div>
-                                        <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">Username</label>
+                                        <span class="block text-sm font-medium text-gray-700 dark:text-gray-300">Username</span>
                                         <p class="mt-1 text-sm text-gray-900 dark:text-gray-100">{{ $account->smtp_username }}</p>
                                     </div>
                                 </div>
