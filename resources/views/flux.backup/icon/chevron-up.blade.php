c2946b97e4f5fd69fea6403c957c15f34d44ed83
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 18:41:31 +0000

    Fix: Delete this unreachable code or refactor the code to make it reachable.

--- resources/views/flux.backup/icon/chevron-up.blade.php
+++ resources/views/flux.backup/icon/chevron-up.blade.php
@@ -28,8 +28,6 @@ $classes = Flux::classes('shrink-0')
   <path fill-rule="evenodd" d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z" clip-rule="evenodd"/>
 </svg>
 
-        <?php break; ?>
-
     <?php case ('mini'): ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd"/>
