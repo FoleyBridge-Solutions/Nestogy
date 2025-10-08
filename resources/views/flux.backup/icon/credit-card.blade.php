51b70c8c8e3d1eadcdea8ece7d44eb1ec0f94d55
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 16:44:58 -0500

    Fix: Issue #11450 - Delete this unreachable code or refactor the code... (#2829)
    * Fix: Delete this unreachable code or refactor the code to make it reachable.
    
    * Trigger CI workflows

--- resources/views/flux.backup/icon/credit-card.blade.php
+++ resources/views/flux.backup/icon/credit-card.blade.php
@@ -36,8 +36,6 @@ $classes = Flux::classes('shrink-0')
   <path fill-rule="evenodd" d="M2.5 4A1.5 1.5 0 0 0 1 5.5V6h18v-.5A1.5 1.5 0 0 0 17.5 4h-15ZM19 8.5H1v6A1.5 1.5 0 0 0 2.5 16h15a1.5 1.5 0 0 0 1.5-1.5v-6ZM3 13.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1-.75-.75Zm4.75-.75a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5h-3.5Z" clip-rule="evenodd"/>
 </svg>
 
-        <?php break; ?>
-
     <?php case ('micro'): ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5V5h14v-.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
