72664e71d8446727e0df229b28090fb34da95991
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 18:22:38 -0500

    Fix: Add a "case default" clause to this "switch" statement. (#2297)

--- app/Domains/Client/Controllers/CredentialController.php
+++ app/Domains/Client/Controllers/CredentialController.php
@@ -305,6 +305,8 @@ class CredentialController extends Controller
                 case 'expiring_soon':
                     $query->expiringSoon();
                     break;
+                default:
+                    break;
             }
         }
 
