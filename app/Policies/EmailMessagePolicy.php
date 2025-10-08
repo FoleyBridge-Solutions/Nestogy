dd70de461c0087276721bdd40ce3b233bdd9ffa8
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 07:34:16 +0000

    Fix: Remove the unused function parameter "$user".

--- app/Policies/EmailMessagePolicy.php
+++ app/Policies/EmailMessagePolicy.php
@@ -10,7 +10,7 @@ class EmailMessagePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(User $_user): bool
     {
         return true;
     }
