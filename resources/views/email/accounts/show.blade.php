b965b7b320142915abf39089eb243105c6217c9e
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 17:38:06 -0500

    Fix: A form label must be associated with a control. (#2707)

--- resources/views/email/accounts/show.blade.php
+++ resources/views/email/accounts/show.blade.php
@@ -180,7 +180,7 @@
                                         <p class="mt-1 text-sm text-gray-900 dark:text-gray-100">{{ $account->auto_create_tickets ? 'Enabled' : 'Disabled' }}</p>
                                     </div>
                                     <div>
-                                        <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">Auto-log Communications</label>
+                                        <span class="block text-sm font-medium text-gray-700 dark:text-gray-300">Auto-log Communications</span>
                                         <p class="mt-1 text-sm text-gray-900 dark:text-gray-100">{{ $account->auto_log_communications ? 'Enabled' : 'Disabled' }}</p>
                                     </div>
                                     <div>
