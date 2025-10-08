ec446f738bd99e6b2da3ff1b444dcf36ca1d1b44
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 18:54:22 +0000

    Fix: Delete this unreachable code or refactor the code to make it reachable.

--- resources/views/flux.backup/icon/cloud.blade.php
+++ resources/views/flux.backup/icon/cloud.blade.php
@@ -34,10 +34,7 @@ $classes = Flux::classes('shrink-0')
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path d="M1 12.5A4.5 4.5 0 0 0 5.5 17H15a4 4 0 0 0 1.866-7.539 3.504 3.504 0 0 0-4.504-4.272A4.5 4.5 0 0 0 4.06 8.235 4.502 4.502 0 0 0 1 12.5Z"/>
 </svg>
-
-        <?php break; ?>
-
-    <?php case ('micro'): ?>
+<?php break; case ('micro'): ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path d="M1 9.5A3.5 3.5 0 0 0 4.5 13H12a3 3 0 0 0 .917-5.857 2.503 2.503 0 0 0-3.198-3.019 3.5 3.5 0 0 0-6.628 2.171A3.5 3.5 0 0 0 1 9.5Z"/>
 </svg>
