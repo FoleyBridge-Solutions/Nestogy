bd5f9cdcc78598bc107cc80605c179a2b2b3569c
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 16:46:08 -0500

    Fix: Issue #11448 - Delete this unreachable code or refactor the code... (#2822)
    * Fix: Delete this unreachable code or refactor the code to make it reachable.
    
    * Trigger CI workflows

--- resources/views/flux.backup/icon/credit-card.blade.php
+++ resources/views/flux.backup/icon/credit-card.blade.php
@@ -29,8 +29,6 @@ $classes = Flux::classes('shrink-0')
   <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd"/>
 </svg>
 
-        <?php break; ?>
-
     <?php case ('mini'): ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path fill-rule="evenodd" d="M2.5 4A1.5 1.5 0 0 0 1 5.5V6h18v-.5A1.5 1.5 0 0 0 17.5 4h-15ZM19 8.5H1v6A1.5 1.5 0 0 0 2.5 16h15a1.5 1.5 0 0 0 1.5-1.5v-6ZM3 13.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1-.75-.75Zm4.75-.75a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5h-3.5Z" clip-rule="evenodd"/>
