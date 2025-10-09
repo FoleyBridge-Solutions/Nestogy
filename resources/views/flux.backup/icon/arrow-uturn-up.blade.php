94899bacbf6be7171282a98d0fff473350be5741
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 22:18:41 -0500

    Fix: Remove the parentheses from this "case" call. (#504)

--- resources/views/flux.backup/icon/arrow-uturn-up.blade.php
+++ resources/views/flux.backup/icon/arrow-uturn-up.blade.php
@@ -16,7 +16,7 @@ $classes = Flux::classes('shrink-0')
     });
 @endphp
 
-<?php switch ($variant): case ('outline'): ?>
+<?php switch ($variant): case 'outline': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
   <path stroke-linecap="round" stroke-linejoin="round" d="m9 9 6-6m0 0 6 6m-6-6v12a6 6 0 0 1-12 0v-3"/>
 </svg>
