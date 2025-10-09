676bc020046253d2f8799acd66fb60200e5462d2
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 20:38:05 -0500

    Fix: Remove the parentheses from this "case" call. (#1015)

--- resources/views/flux.backup/icon/chevron-up.blade.php
+++ resources/views/flux.backup/icon/chevron-up.blade.php
@@ -16,14 +16,14 @@ $classes = Flux::classes('shrink-0')
     });
 @endphp
 
-<?php switch ($variant): case ('outline'): ?>
+<?php switch ($variant): case 'outline': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
   <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
 </svg>
 
         <?php break; ?>
 
-    <?php case ('solid'): ?>
+    <?php case 'solid': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path fill-rule="evenodd" d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z" clip-rule="evenodd"/>
 </svg>
