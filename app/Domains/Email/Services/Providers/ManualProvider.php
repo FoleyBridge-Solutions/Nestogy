42da99f874fd080653b700e52a7cb51ff17a477d
Andrew Malsbury <andymalsb@gmail.com>
2025-10-09 10:12:55 -0500

    Fix: Define a constant instead of duplicating this literal "Manual provider does not support OAuth" 3 times. (#195)

--- app/Domains/Email/Services/Providers/ManualProvider.php
+++ app/Domains/Email/Services/Providers/ManualProvider.php
@@ -33,19 +33,19 @@ class ManualProvider implements EmailProviderInterface
     public function getAuthorizationUrl(string $state): string
     {
         // Manual provider doesn't use OAuth
-        throw new \Exception('Manual provider does not support OAuth');
+        throw new \Exception(self::OAUTH_NOT_SUPPORTED_MESSAGE);
     }
 
     public function exchangeCodeForTokens(string $code): array
     {
         // Manual provider doesn't use OAuth
-        throw new \Exception('Manual provider does not support OAuth');
+        throw new \Exception(self::OAUTH_NOT_SUPPORTED_MESSAGE);
     }
 
     public function refreshTokens(string $refreshToken): array
     {
         // Manual provider doesn't use OAuth
-        throw new \Exception('Manual provider does not support OAuth');
+        throw new \Exception(self::OAUTH_NOT_SUPPORTED_MESSAGE);
     }
 
     public function getAccountData(array $tokens, string $email): array
