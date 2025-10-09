85ff03ce84376480a174f7a32b9e44de8e75f8b2
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 20:07:14 -0500

    Fix: Remove the parentheses from this "case" call. (#1796)

--- resources/views/flux.backup/icon/slash.blade.php
+++ resources/views/flux.backup/icon/slash.blade.php
@@ -16,21 +16,21 @@ $classes = Flux::classes('shrink-0')
     });
 @endphp
 
-<?php switch ($variant): case ('outline'): ?>
+<?php switch ($variant): case 'outline': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
   <path stroke-linecap="round" stroke-linejoin="round" d="m9 20.247 6-16.5"/>
 </svg>
 
         <?php break; ?>
 
-    <?php case ('solid'): ?>
+    <?php case 'solid': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path fill-rule="evenodd" d="M15.256 3.042a.75.75 0 0 1 .449.962l-6 16.5a.75.75 0 1 1-1.41-.513l6-16.5a.75.75 0 0 1 .961-.449Z" clip-rule="evenodd"/>
 </svg>
 
         <?php break; ?>
 
-    <?php case ('mini'): ?>
+    <?php case 'mini': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path fill-rule="evenodd" d="M12.528 3.047a.75.75 0 0 1 .449.961L8.433 16.504a.75.75 0 1 1-1.41-.512l4.544-12.496a.75.75 0 0 1 .961-.449Z" clip-rule="evenodd"/>
 </svg>
