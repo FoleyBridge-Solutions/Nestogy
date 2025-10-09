bb872dc425db2969ce5e74aca3099ed89e0e7f3e
Andrew Malsbury <andymalsb@gmail.com>
2025-10-08 20:28:35 -0500

    Fix: Remove the parentheses from this "case" call. (#1267)

--- resources/views/flux.backup/icon/equals.blade.php
+++ resources/views/flux.backup/icon/equals.blade.php
@@ -30,7 +30,7 @@ $classes = Flux::classes('shrink-0')
 
         <?php break; ?>
 
-    <?php case ('mini'): ?>
+    <?php case 'mini': ?>
 <svg {{ $attributes->class($classes) }} data-flux-icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
   <path d="M3.75 6a.75.75 0 0 0 0 1.5h12.5a.75.75 0 0 0 0-1.5H3.75ZM3.75 13.5a.75.75 0 0 0 0 1.5h12.5a.75.75 0 0 0 0-1.5H3.75Z"/>
 </svg>
