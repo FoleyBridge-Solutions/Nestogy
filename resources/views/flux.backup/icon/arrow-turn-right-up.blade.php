687056b6f083fa56116f61aaac8953469c550ee9
Andrew Malsbury <andymalsb@gmail.com>
2025-10-10 22:14:12 +0000

    Fix:

--- resources/views/flux.backup/icon/arrow-turn-right-up.blade.php
+++ resources/views/flux.backup/icon/arrow-turn-right-up.blade.php
@@ -49,6 +49,4 @@ $classes = Flux::classes('shrink-0')
   <path stroke-linecap="round" stroke-linejoin="round" d="m11.99 7.5 3.75-3.75m0 0 3.75 3.75m-3.75-3.75v16.499H4.49"/>
 </svg>
 
-        <?php break; ?>
-
 <?php endswitch; ?>
