4e4c4ff88fd42051cc137857cf7eaa42e285c765
o-psi <andymalsb@gmail.com>
2025-10-14 19:55:09 +0000

    Remove duplicate header text from invoice create page

--- resources/views/financial/invoices/create-livewire.blade.php
+++ resources/views/financial/invoices/create-livewire.blade.php
@@ -6,9 +6,6 @@ $pageTitle = 'Create Invoice';
 
 @section('content')
     <div class="container-fluid">
-        <div class="mb-6">
-            <p class="text-gray-500">Create a new invoice for billing</p>
-        </div>
         @livewire('financial.invoice-create')
     </div>
 @endsection
